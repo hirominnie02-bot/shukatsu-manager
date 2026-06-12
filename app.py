e5bd050b4fce91804f385e1f666a1fb3320ca270
Degu <268776314+hirominnie02-bot@users.noreply.github.com>
2026-06-12 14:17:26 +0900

    Add duplicate check and improve company list display

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
 import streamlit as st
+import sqlite3 
+#軽量データベースインポート
 
 st.title("就活管理アプリ")
 
 company_name = st.text_input("会社名")
 
-if st.button("保存"):
-    st.write("保存しました！")
-    st.write(company_name)
+if st.button("保存"): #保存ボタンが押された時の処理
+    #データベースjob_app.dbに接続
+    conn = sqlite3.connect("job_app.db")
+
+    #SQL実行担当作成
+    cursor = conn.cursor()
+
+    #SQL実行命令
+    cursor.execute(
+        "INSERT INTO companies (company_name) VALUES (?)", 
+        # 第一引数：実行するSQL文
+        # companiesテーブルのcompany_name列にデータを追加する
+        (company_name,), 
+        # 第二引数：プレースホルダー(?)に渡す実際の値
+    )
+
+    cursor.execute("SELECT * FROM companies")
+    rows = cursor.fetchall()
+    st.write(rows)
+
+    #データベースの変更を確定
+    conn.commit()
+
+    #データベースの接続を終了する
+    conn.close()
